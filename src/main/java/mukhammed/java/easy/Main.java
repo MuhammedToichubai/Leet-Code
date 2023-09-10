a85907751f6276e88465087b3c3010284e614107
mukhammedasantegin <muhammedtoichubai@gmail.com>
2023-09-10 14:55:05 +0600

    add digits method option 2

--- src/main/java/mukhammed/java/easy/Main.java
+++ src/main/java/mukhammed/java/easy/Main.java
@@ -6,7 +6,11 @@ public class Main {
     public static void main(String[] args) {
         // TODO: 9/9/23  Task: 1
         System.out.println(addDigits(new Scanner(System.in).nextInt()));
+        System.out.println(addDigits(new Scanner(System.in).nextInt()));
+
     }
+
+    // TODO: option 1
     public static int addDigits(int num) {
         if (0 <= num) {
             if (num >= 10) {
@@ -20,5 +24,17 @@ public class Main {
         }
         return num;
     }
+
+    // TODO: option 2
+    public static int addDigits(Integer num){
+        if (num < 10) return num;
+        int newNum = 0;
+        while (num > 0){
+            newNum += (num % 10);
+            num /= 10;
+        }
+        return newNum < 10 ? newNum : addDigits(newNum);
+    }
+
 
 }
